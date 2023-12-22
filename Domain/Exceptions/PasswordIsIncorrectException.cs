c30addf36467a89213b15165753afc7b0a5eb9ce
Maksym <m.datsenko@pnn.in.ua>
2023-12-22 17:36:35 +0200

    Add entities and exceptions.

---
+++ Domain/Exceptions/PasswordIsIncorrectException.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Exceptions;
+
+public sealed class PasswordIsIncorrectException : Exception
+{
+    public PasswordIsIncorrectException() : base("Password is not correct.")
+    {
+    }
+}
---
+++ Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Exceptions;
+
+public sealed class UserNotFoundException : Exception
+{
+    public UserNotFoundException() : base("User with this login was not found.")
+    {
+    }
+}

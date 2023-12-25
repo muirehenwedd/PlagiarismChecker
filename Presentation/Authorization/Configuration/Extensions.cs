55386585ac3cea2e639d894f448f8a5c42cb38c5
Maksym <m.datsenko@pnn.in.ua>
2023-12-25 17:28:48 +0200

    Add authorization policies.

---
+++ Presentation/Authorization/Configuration/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Presentation.Authorization.Policies;
+
+namespace Presentation.Authorization.Configuration;
+
+public static class Extensions
+{
+    public static IServiceCollection RegisterAuthorization(this IServiceCollection collection)
+    {
+        collection.AddAuthorizationBuilder()
+            .AddPolicy(AdminPolicy.Name, new AdminPolicy())
+            .AddPolicy(UserPolicy.Name, new UserPolicy());
+
+        return collection;
+    }
+}
---
+++ Presentation/Authorization/Policies/UserPolicy.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Presentation.Authentication;
+
+namespace Presentation.Authorization.Policies;
+
+public sealed class UserPolicy : AuthorizationPolicy
+{
+    public const string Name = "User";
+
+    private static readonly IEnumerable<IAuthorizationRequirement> requirements = new[]
+    {
+        new DenyAnonymousAuthorizationRequirement()
+    };
+
+    private static readonly IEnumerable<string> authenticationSchemes = new[]
+    {
+        JwtAuth.SchemeName
+    };
+
+    public UserPolicy() : base(requirements, authenticationSchemes)
+    {
+    }
+}

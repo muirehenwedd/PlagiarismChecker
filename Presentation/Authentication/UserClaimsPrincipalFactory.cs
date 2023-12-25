a5cdd571252c4ab7249eaa5cd29a201543424295
Maksym <m.datsenko@pnn.in.ua>
2023-12-25 17:28:28 +0200

    Add authentication handlers.

---
+++ Presentation/Authentication/UserClaimsPrincipalFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace Presentation.Authentication;
+
+public sealed class ClaimsPrincipalFactory : UserClaimsPrincipalFactory<User>
+{
+    public ClaimsPrincipalFactory(UserManager<User> userManager, IOptions<IdentityOptions> optionsAccessor)
+        : base(userManager, optionsAccessor)
+    {
+    }
+
+    public override async Task<ClaimsPrincipal> CreateAsync(User user)
+    {
+        var claims = await base.GenerateClaimsAsync(user);
+
+        throw new NotImplementedException();
+    }
+}
